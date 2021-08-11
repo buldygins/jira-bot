c922e8a9116742e6d5c64b4933c4223145d4c5b2
Вениамин Смородинский <origamiv@gmail.com>
2021-08-11 19:47:03 +0300

    jira

--- database/migrations/2021_08_11_140951_create_jira_issues_table.php
+++ database/migrations/2021_08_11_140951_create_jira_issues_table.php
@@ -18,6 +18,7 @@ class CreateJiraIssuesTable extends Migration
             $table->string('key')->comment('Jira issue key');
             $table->string('summary')->nullable()->comment('Название задачи');
             $table->string('issue_url')->nullable()->comment('URL задачи');
+            $table->integer('issue_id')->nullable()->comment('ID задачи в Жире');
             $table->string('webhookEvent')->nullable()->comment('Событие');
             $table->string('updateAuthor')->nullable()->comment('Автор события');
             $table->dateTime('added')->nullable()->comment('дата добавления задачи');
