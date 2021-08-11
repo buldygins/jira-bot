bbb18989e7baf4b7d67c782a2f349204bc079893
Вениамин Смородинский <origamiv@gmail.com>
2021-08-12 02:29:57 +0300

    Позиции

--- app/Handlers/Command/SetPositionCommand.php
+++ app/Handlers/Command/SetPositionCommand.php
@@ -7,24 +7,24 @@ use WeStacks\TeleBot\Handlers\CommandHandler;
 
 class SetPositionCommand extends CommandHandler
 {
-    protected static $aliases = [ '/set_position'];
+    protected static $aliases = ['/set_position'];
     protected static $description = 'Задать мою должность';
 
     public function handle()
     {
         $chat_id = $this->update->message->chat->id;
 
-        $sub=Subscriber::query()
+        $sub = Subscriber::query()
             ->where('chat_id', '=', $chat_id)
             ->first();
 
 
-        $sub->waited_command='SetPositionCommand';
-            $sub->save();
+        $sub->waited_command = 'SetPositionCommand';
+        $sub->save();
 
 
         $this->sendMessage([
-            'text' => 'Задайте свою должность '. $sub->id
+            'text' => 'Задайте свою должность ' . $sub->id
             //$chat_id,
         ]);
         return true;
