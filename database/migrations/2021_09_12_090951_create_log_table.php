d33ae1a69ce074e2e946db0a29391be58fdca068
Вениамин Смородинский <origamiv@gmail.com>
2021-08-12 11:55:01 +0300

    логи

---
+++ database/migrations/2021_09_12_090951_create_log_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Database\Seeders\PositionSeeder;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->id();
+            $table->integer('issue_id')->nullable();
+            $table->string('issue_key')->nullable();
+            $table->string('webhook_event')->nullable();
+            $table->string('name')->nullable()->comment('Сообщение');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+}
