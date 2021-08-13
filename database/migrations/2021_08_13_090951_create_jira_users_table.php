806bd447ed0c1ed03af87cf3fb3094c9dcfc8fd2
Вениамин Смородинский <origamiv@gmail.com>
2021-08-13 11:09:24 +0300

    jiraUsers

--- database/migrations/2021_08_13_090951_create_jira_users_table.php
+++ database/migrations/2021_08_13_090951_create_jira_users_table.php
@@ -17,7 +17,7 @@ class CreateJiraUsersTable extends Migration
     {
         Schema::create('jira_users', function (Blueprint $table) {
             $table->id();
-            $table->integer('key')->nullable();
+            $table->string('key')->nullable();
             $table->string('accountId')->nullable();
             $table->string('name')->nullable();
             $table->boolean('active')->nullable();
