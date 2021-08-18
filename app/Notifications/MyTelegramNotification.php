2fff5ff4e474ba456f2e42901427566d14f32cf7
Вениамин Смородинский <origamiv@gmail.com>
2021-08-18 18:45:50 +0300

    fix

--- app/Notifications/MyTelegramNotification.php
+++ app/Notifications/MyTelegramNotification.php
@@ -64,7 +64,7 @@ class MyTelegramNotification extends Notification
             'event_processed' => $this->issue->event_created
         ]);
 
-        $link = config('jira_host') . '/browse/' . $this->issue->key;
+        $link = config('app.jira_host') . '/browse/' . $this->issue->key;
 
         $message = [
             'parse_mode' => 'HTML',
