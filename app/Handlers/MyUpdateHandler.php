328b6030272a2cb47c7db29d3ed98d5f16e98c39
Вениамин Смородинский <origamiv@gmail.com>
2021-08-18 17:28:56 +0300

    fix

--- app/Handlers/MyUpdateHandler.php
+++ app/Handlers/MyUpdateHandler.php
@@ -17,25 +17,6 @@ class MyUpdateHandler extends UpdateHandler
 
     public function command($cmd, Subscriber $subscriber)
     {
-
-        $r = $this->getLocalCommands();
-        foreach ($r as $obj) {
-            $item = $obj->toArray();
-            $cmd1 = str_replace($item['command'] . '_', '', $cmd);
-            if ($cmd1 != $cmd) {
-//                    $this->sendMessage([
-//                        'text' => ' Команда: ' . $item['command'] .' id '. $cmd2,
-//                    ]);
-
-                if ($item['command'] == '/set_position') {
-                    $commandHandler = new SetPositionCommand($this->bot, $this->update);
-                    $commandHandler->answer($cmd1);
-                }
-
-            }
-        }
-
-
         //=== далее = ответы на команды
 
         if (!is_null($subscriber->waited_command)) {
@@ -50,6 +31,32 @@ class MyUpdateHandler extends UpdateHandler
                     return true;
                 }
             }
+        }
+
+        $fn = str_replace('/', '', $cmd);
+        if ($fn != $cmd) {
+            // команды
+
+            $cmd1 = $cmd;
+
+            $r = $this->getLocalCommands();
+            foreach ($r as $obj) {
+                $item = $obj->toArray();
+                $cmd2 = str_replace($item['command'] . '_', '', $cmd1);
+                if ($cmd2 != $cmd1) {
+//                    $this->sendMessage([
+//                        'text' => ' Команда: ' . $item['command'] .' id '. $cmd2,
+//                    ]);
+
+                    if ($item['command'] == '/set_position') {
+                        $commandHandler = new SetPositionCommand($this->bot, $this->update);
+                        $commandHandler->answer($cmd2);
+                    }
+
+                }
+            }
+
+            return true;
         }
 
         return true;
