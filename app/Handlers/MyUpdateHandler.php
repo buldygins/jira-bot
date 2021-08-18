b830d1769486ff8d3dde3886a377c919eb30f87b
Вениамин Смородинский <origamiv@gmail.com>
2021-08-18 17:29:49 +0300

    fix

--- app/Handlers/MyUpdateHandler.php
+++ app/Handlers/MyUpdateHandler.php
@@ -17,6 +17,22 @@ class MyUpdateHandler extends UpdateHandler
 
     public function command($cmd, Subscriber $subscriber)
     {
+
+        $fn = str_replace('/', '', $cmd);
+        if ($fn != $cmd) {
+            // команды
+
+            $cmd1 = $cmd;
+
+            $r = $this->getLocalCommands();
+            foreach ($r as $obj) {
+                $item = $obj->toArray();
+                $cmd2 = str_replace($item['command'] . '_', '', $cmd1);
+            }
+
+            return true;
+        }
+
         //=== далее = ответы на команды
 
         if (!is_null($subscriber->waited_command)) {
@@ -33,31 +49,7 @@ class MyUpdateHandler extends UpdateHandler
             }
         }
 
-        $fn = str_replace('/', '', $cmd);
-        if ($fn != $cmd) {
-            // команды
 
-            $cmd1 = $cmd;
-
-            $r = $this->getLocalCommands();
-            foreach ($r as $obj) {
-                $item = $obj->toArray();
-                $cmd2 = str_replace($item['command'] . '_', '', $cmd1);
-                if ($cmd2 != $cmd1) {
-//                    $this->sendMessage([
-//                        'text' => ' Команда: ' . $item['command'] .' id '. $cmd2,
-//                    ]);
-
-                    if ($item['command'] == '/set_position') {
-                        $commandHandler = new SetPositionCommand($this->bot, $this->update);
-                        $commandHandler->answer($cmd2);
-                    }
-
-                }
-            }
-
-            return true;
-        }
 
         return true;
     }
