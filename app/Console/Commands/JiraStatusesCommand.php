1f9ae6adb88d35412c065444b42b40e1781bf6d8
Вениамин Смородинский <origamiv@gmail.com>
2021-08-19 12:35:10 +0300

    fix

--- app/Console/Commands/JiraStatusesCommand.php
+++ app/Console/Commands/JiraStatusesCommand.php
@@ -48,6 +48,7 @@ class JiraStatusesCommand extends Command
             'jiraHost' => config('app.jira_url'),
             'jiraUser' => $user->jira_login,
             'jiraPassword' => $user->api_token,
+            'useV3RestApi' => true,
         ]);
         dump($config);
         $projectService = new ProjectService($config);
